08da0a08b3ca0d2b08a41f1b5a5eafb8973a4fc2
Matheus Balduino <matheus_mgb@hotmail.com>
2021-08-15 10:41:50 -0300

    Add project files.

---
+++ No_EF_Api/Controllers/NoEnFrApiController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using No_EF_Api.Model;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace No_EF_Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class NoEnFrApiController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public NoEnFrApiController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpPost]
+        public IActionResult addUser(Users user)
+        {
+
+            SqlConnection sqlConnection = null;
+            try
+            {
+
+                sqlConnection = new SqlConnection(_configuration.GetConnectionString("testeApi"));
+                sqlConnection.Open();
+                SqlCommand cmd = new SqlCommand("addUser", sqlConnection);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("Nome", user.Nome);
+                cmd.Parameters.AddWithValue("Obs", user.Obs);
+
+                cmd.ExecuteNonQuery();
+
+                return Ok("Adicionado com Sucesso");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+
+            }
+            finally
+            {
+                sqlConnection.Close();
+            }
+        }
+    }
+}
---
+++ No_EF_Api/Model/Users.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace No_EF_Api.Model
+{
+    public class Users
+    {
+        public int UserId { get; set; }
+        public string Nome { get; set; }
+        public string Obs { get; set; }
+    }
+}
